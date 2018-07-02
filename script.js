d9b8e8e38e05e8f4d77e47689b578b5f32d77400
Jenna <pinkhamjenna@gmail.com>
2018-07-02 16:09:06 -0700

    added guessing-game functionality

--- script.js
+++ script.js
@@ -7,15 +7,29 @@ $(document).ready(function(){
             var row = $('<tr>');
 
             for(i=1; i<=18; i++){
-                var cell = $('<td>')
+                var cell = $('<td>');
                 if(elements[k][i] != undefined){
-                    cell.html(elements[k][i]['symbol']);
-                    cell.addClass(elements[k][i]['orbital'])
+                    var span = $('<span>')
+                    cell.html(span.html(elements[k][i]['symbol']));
+                    cell.attr('id', elements[k][i]['symbol']);
+                    cell.addClass(elements[k][i]['orbital']);
                 }
                 row.append(cell);
             }
             table.append(row);
+            $('span').hide()
         }
     }
+    // Listens for elements in the guess box
+    $('#guess-box').on('input', function(){
+        var guess = $('#guess-box').val()
+        for (i=0; i<elementList.length; i++) {
+            if(guess.toUpperCase() == elementList[i][1].toUpperCase()){
+                $('#guess-box').val('');
+                var str = '#' + elementList[i][0] + ' span';
+                $(str).show();
+            }
+        }
+    });
     
 });
